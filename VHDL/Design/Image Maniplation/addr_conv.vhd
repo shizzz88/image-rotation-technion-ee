eacd22e3c9cc4a681afceb5cfd700fb869556449
ran.mizrahy <ran.mizrahy@b4713c3a-7e5d-8e7b-b65c-164a7b9bc44e>
2012-03-27 16:31:09 +0000

    addr_conv.vhd - first test

---
+++ VHDL/Design/Image Maniplation/addr_conv.vhd
@@ -0,0 +1,121 @@
+------------------------------------------------------------------------------------------------
+-- Model Name 	:	Address Converter
+-- File Name	:	Addr_Conv.vhd
+-- Generated	:	27.03.1012
+-- Author		:	Ran Mizrahi
+-- Project		:	Im_rotate Project
+------------------------------------------------------------------------------------------------
+-- Description  :   converts matrix address [I,J] to SDRAM address (continuous)
+------------------------------------------------------------------------------------------------
+-- Revision:
+--			Number		Date		Name					Description			
+--			1.00		27.3.2012	Ran&Uri					creation
+------------------------------------------------------------------------------------------------
+--	Todo:
+--			(1) 		
+------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use ieee.math_real.all;
+
+library work ;
+--use work.ram_generic_pkg.all;
+
+entity addr_conv is
+	generic (
+			x_size_in				:	positive 	:= 96;				-- number of rows  in the input image
+			y_size_in				:	positive 	:= 128			-- number of columns  in the input image
+			);
+	port	(
+				x_in				:	in std_logic_vector (9 downto 0);		--row index input
+				y_in				:	in std_logic_vector (9 downto 0);		--column index input
+				ram_start_add_in	:	in std_logic_vector (9 downto 0);		--SDram beginning address
+				addr_out			:	out std_logic_vector(9 downto 0);		--address in SDRAM form
+				--Clock and Reset
+				clk_133				:	in std_logic;							--SDRAM clock
+				clk_40				:	in std_logic;							--VESA Clock
+				rst_133				:	in std_logic;							--Reset (133MHz)
+				rst_40				:	in std_logic;							--Reset (40MHz)
+
+				-- Wishbone Master to Memory Management block
+				wbm_dat_i			:	in std_logic_vector (7 downto 0);		--Data in (8 bits)
+				wbm_stall_i			:	in std_logic;							--Slave is not ready to receive new data 
+				wbm_ack_i			:	in std_logic;							--Input data has been successfuly acknowledged
+				wbm_err_i			:	in std_logic;							--Error: Address should be incremental, but receives address was not as expected (0 --> 1023)
+				wbm_adr_o			:	out std_logic_vector (9 downto 0);		--Address
+				wbm_tga_o			:	out std_logic_vector (9 downto 0);		--Address Tag : Read burst length-1 (0 represents 1 byte, 3FF represents 1023 bytes)
+				wbm_cyc_o			:	out std_logic;							--Cycle command from WBM
+				wbm_stb_o			:	out std_logic;							--Strobe command from WBM
+				wbm_tgc_o			:	out std_logic							--Cycle Tag
+			);
+end entity addr_conv;
+
+architecture arc_addr_conv of addr_conv is
+
+--	###########################		Costants		##############################	--
+--constant reg_width_c			:	positive 	:= 8;	--Width of registers
+
+--###########################	Signals		###################################--
+--40MHz Clock Domain Signals
+--signal pixels_req		:	std_logic_vector(integer(ceil(log(real(hor_active_pixels_g*req_lines_g)) / log(2.0))) - 1 downto 0); --Request for PIXELS*LINES pixels from FIFO
+--###########################	Components	###################################--
+
+
+
+-- component synthetic_frame_generator 
+	-- generic (
+			-- green_width_g			:	positive 	:= 8;				--Default std_logic_vector size of Green Pixels
+			-- blue_width_g			:	positive 	:= 8				--Default std_logic_vector size of Blue Pixels
+			-- );
+	-- port	(
+			-- lower_frame				:	out std_logic_vector(integer(ceil(log(real(ver_active_lines_g)) / log(2.0))) - 1 downto 0);		--Lower frame border
+			-- data_valid				: 	out std_logic
+			-- );
+-- end component synthetic_frame_generator;
+
+
+
+begin
+
+addr_out	<=	ram_start_add_in+x_in*conv_std_logic_vector(y_size_in,10) + y_in;
+--addr_out	<=	ram_start_add_in+x_in + y_in;
+
+								
+-- --###########################	Instatiation	###################################--
+
+
+-- pixel_mng_inst: pixel_mng generic map
+						-- (
+							-- reset_polarity_g	=>	reset_polarity_g,	
+							-- vsync_polarity_g	=>	vsync_polarity_g,
+							-- screen_hor_pix_g	=>	hor_active_pixels_g,
+							-- hor_pixels_g		=>	hor_pres_pixels_g,
+							-- ver_lines_g			=>	ver_pres_lines_g,
+							-- req_lines_g			=>	req_lines_g
+-- --							rep_size_g			=>	rep_size_g
+						-- )
+						-- port map
+						-- (
+							-- clk_i				=>	clk_133,		
+							-- rst			        =>	rst_133,
+							-- wbm_ack_i	        =>	wbm_ack_i,
+							-- wbm_err_i	        =>	wbm_err_i	,
+							-- wbm_stall_i	        =>	wbm_stall_i	,
+							-- wbm_dat_i	        =>	wbm_dat_i	,
+							-- wbm_cyc_o	        =>	wbm_cyc_o	,
+							-- wbm_stb_o	        =>	wbm_stb_o	,
+							-- wbm_adr_o	        =>	wbm_adr_o	,
+							-- wbm_tga_o	        =>	wbm_tga_o	,
+							-- fifo_wr_en	        =>	sc_fifo_wr_en,
+							-- fifo_flush	        =>	flush,
+							-- pixels_req	        =>	pixels_req,
+							-- req_ln_trig	        =>	req_ln_trig,
+							-- vsync		        =>	vsync_int		
+						-- );
+
+
+	
+end architecture arc_addr_conv;
