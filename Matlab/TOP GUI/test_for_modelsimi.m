a33b21019a397a9d0d606096f5c533f463b300cd
zipin31 <zipin31@b4713c3a-7e5d-8e7b-b65c-164a7b9bc44e>
2012-05-29 09:48:38 +0000

    fix valid

--- Matlab/TOP GUI/test_for_modelsimi.m
+++ Matlab/TOP GUI/test_for_modelsimi.m
@@ -1,8 +1,10 @@
 clc
 clear all
-teta=0*60/180*pi;
-s=301;%row indx
-t=301;%coloum indx
+%teta=0*60/180*pi;
+teta=60/180*pi;
+%s=304;%col indx
+t=301;%row indx
+s=301;
 HorResOut=800;
 VerResOut=600;
 n=100;
@@ -10,11 +12,16 @@ m=67;
 Xstart=30;
 Ystart=29;
 ZoomFactor=0.25;
-i =(  ZoomFactor*(t-VerResOut/2)*cos(teta) +  ZoomFactor*(s-HorResOut/2)*sin(teta)+m/2)
-j =(  -ZoomFactor*(t-VerResOut/2)*sin(teta) +ZoomFactor* (s-HorResOut/2)*cos(teta)+n/2)
+result=zeros(30,3);
+for index=1:1:30
 
-i=i+Xstart 
-j=j+Ystart
+result(index,1)=s;
+i =(  ZoomFactor*(t-VerResOut/2)*cos(teta) +  ZoomFactor*(s-HorResOut/2)*sin(teta)+m/2);
+j =(  -ZoomFactor*(t-VerResOut/2)*sin(teta) +ZoomFactor* (s-HorResOut/2)*cos(teta)+n/2);
 
-
-tl_ram=(floor(i)-1)*128+floor(j)
+i=i+Xstart ;
+j=j+Ystart;
+result(index,2)=i;
+result(index,3)=j;
+s=s+1;
+end
